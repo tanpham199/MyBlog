c4a7a8e009ed8dd6b73e04c362f708b1a934f530
Tân Phạm <tanpham1104@gmail.com>
2021-02-16 14:49:15 +0700

    chore: reformat code

--- Client/src/components/ErrorHandler/ErrorHandler.js
+++ Client/src/components/ErrorHandler/ErrorHandler.js
@@ -3,20 +3,20 @@ import React, { Fragment } from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import Modal from '../Modal/Modal';
 
-const errorHandler = props => (
-  <Fragment>
-    {props.error && <Backdrop onClick={props.onHandle} />}
-    {props.error && (
-      <Modal
-        title="An Error Occurred"
-        onCancelModal={props.onHandle}
-        onAcceptModal={props.onHandle}
-        acceptEnabled
-      >
-        <p>{props.error.message}</p>
-      </Modal>
-    )}
-  </Fragment>
+const errorHandler = (props) => (
+    <Fragment>
+        {props.error && <Backdrop onClick={props.onHandle} />}
+        {props.error && (
+            <Modal
+                title="An Error Occurred"
+                onCancelModal={props.onHandle}
+                onAcceptModal={props.onHandle}
+                acceptEnabled
+            >
+                <p>{props.error.message}</p>
+            </Modal>
+        )}
+    </Fragment>
 );
 
 export default errorHandler;
