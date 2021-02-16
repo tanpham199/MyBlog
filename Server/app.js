96eafd04dbb46841a424982176f437f54182639d
Tân Phạm <tanpham1104@gmail.com>
2021-02-16 14:46:55 +0700

    feat: retrieve data from front-end

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
@@ -13,5 +16,17 @@ app.use((req, res, next) => {
     next();
 });
 app.use('/feed', feedRoutes);
+app.use((error, req, res, next) => {
+    res.status(error.statusCode || 500).json({ message: error.message });
+});
 
-app.listen(8080);
+mongoose
+    .connect('mongodb+srv://tan:a@cluster0.b8khd.mongodb.net/message?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        app.listen(8080);
+    })
+    .catch((err) => console.log(err));
--- Server/controllers/feed.js
+++ Server/controllers/feed.js
@@ -1,11 +1,44 @@
+const { validationResult } = require('express-validator');
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({ posts: [{ title: 'First Post', content: 'This is a post' }] });
+    res.status(200).json({
+        posts: [
+            {
+                _id: '1',
+                title: 'First Post',
+                content: 'This is a post',
+                imageUrl: 'images/raccoon.jpeg',
+                creator: { name: 'Tân Phạm' },
+                createdAt: new Date(),
+            },
+        ],
+    });
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrent.');
+        error.statusCode = 422;
+        next(error);
+    }
     const { title, content } = req.body;
-    res.status(201).json({
-        message: 'Post created successfully.',
-        post: { id: new Date().toISOString(), title, content },
+    const post = new Post({
+        title,
+        content,
+        imageUrl: 'images/raccoon.jpeg',
+        creator: { name: 'Tân Phạm' },
     });
+    try {
+        res.status(201).json({
+            message: 'Post created successfully.',
+            post: await post.save(),
+        });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 };
